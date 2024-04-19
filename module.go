098b8fadca3408761e8a391a634b7ac75f0e55f5
kmou424 <me@kmou424.moe>
2024-04-19 11:16:12 +0000

    feat: introduce simple module

--- module.go
+++ module.go
@@ -3,3 +3,21 @@ package dingtalkbot
 type Module interface {
 	handlers() (middlewares []HandlerFunc, handler HandlerFunc)
 }
+
+// Simple only one handler module
+type Simple struct {
+	handler HandlerFunc
+}
+
+func NewSimple() *Simple {
+	return &Simple{}
+}
+
+func (s *Simple) Handle(handler HandlerFunc) *Simple {
+	s.handler = handler
+	return s
+}
+
+func (s *Simple) handlers() ([]HandlerFunc, HandlerFunc) {
+	return nil, s.handler
+}
